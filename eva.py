e31be8ecd0945b22124f49c730fbee80b19cc4bf
Tekk <tekk@MacBook-Pro.local>
2020-12-08 14:45:48 +0100

    Init

---
+++ eva.py
@@ -0,0 +1,24 @@
+from google.cloud import texttospeech
+
+client = texttospeech.TextToSpeechClient()
+
+input_text = texttospeech.SynthesisInput(text="Ahoj, tu je Eva.")
+
+# Note: the voice can also be specified by name.
+# Names of voices can be retrieved with client.list_voices().
+voice = texttospeech.VoiceSelectionParams(
+    language_code="sk-SK", ssml_gender=texttospeech.SsmlVoiceGender.FEMALE
+)
+
+audio_config = texttospeech.AudioConfig(
+    audio_encoding=texttospeech.AudioEncoding.MP3
+)
+
+response = client.synthesize_speech(
+    request={"input": input_text, "voice": voice, "audio_config": audio_config}
+)
+
+# The response's audio_content is binary.
+with open("output.mp3", "wb") as out:
+    out.write(response.audio_content)
+    print('Audio content written to file "output.mp3"')
---
+++ list_voices.py
@@ -0,0 +1,15 @@
+from google.cloud import texttospeech
+
+
+def list_voices(language_code='sk'):
+    client = texttospeech.TextToSpeechClient()
+    response = client.list_voices(language_code=language_code)
+    voices = sorted(response.voices, key=lambda voice: voice.name)
+
+    print(f" Voices: {len(voices)} ".center(60, "-"))
+    for voice in voices:
+        languages = ", ".join(voice.language_codes)
+        name = voice.name
+        gender = texttospeech.SsmlVoiceGender(voice.ssml_gender).name
+        rate = voice.natural_sample_rate_hertz
+        print(f"{languages:<8} | {name:<24} | {gender:<8} | {rate:,} Hz")
